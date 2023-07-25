5e5899e1527b3f2f7fa8427256acb47161432abd
Mohammed Amine Birje <128231064+AmineACG@users.noreply.github.com>
2023-07-25 02:11:56 +0100

    Changing the Profile page layout
    - Let the user observe his/her profile 
    Need to make the edit.php

--- profile.php
+++ profile.php
@@ -1,14 +1,7 @@
-
 <?php
 session_start();
+// Check if the user is not logged in and redirect to the login page if necessary
 
-// Check if the user is already logged in
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
-
-// Connect to the database
 $dsn = 'mysql:host=localhost;dbname=fitfuelhub_db';
 $username = 'root';
 $password = '';
@@ -16,148 +9,210 @@ $password = '';
 try {
     $db = new PDO($dsn, $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "SELECT * FROM recipes";
+    $stmt = $db->query($query);
+    $recipes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $query = "SELECT * FROM custom_meal";
+    $stmt = $db->query($query);
+    $customMeals = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 } catch (PDOException $e) {
     echo 'Database Error: ' . $e->getMessage();
     die();
 }
-
-// Retrieve user data from the database
-$query = "SELECT * FROM user WHERE user_id = :user_id";
-$stmt = $db->prepare($query);
-$stmt->execute([':user_id' => $_SESSION['user_id']]);
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-// Assign user data to variables
-$fullName = $user['username'];
-$email = $user['email'];
-$gender = $user['sex'];
-$birthday = $user['birthday'];
-$height = $user['height'];
-$weight = $user['weight'];
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Profile</title>
+    <title>Recipe Page</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+            background-color: #f2f2f2;
+        }
+
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem 0;
+        }
+
+        .section-content {
+        display: flex;
+    }
+
+    .left-portion {
+        flex: 1;
+        max-width: 70%;
+        height: 100%;
+        background-color: #fff;
+        border-radius: 5px;
+        padding: 1rem;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+
+    .right-portion {
+        flex: 1;
+        max-width: 30%; /* Adjust the max-width to your preference */
+        height: 100%;
+        background-color: #fff;
+        border-radius: 5px;
+        padding: 1rem;
+        box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+    }
+        .container {
+            max-width: 960px;
+            margin: 0 auto;
+        }
+
+        .recipe {
+            display: flex;
+            border-bottom: 1px solid #ccc;
+            padding-bottom: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        .recipe:last-child {
+            border-bottom: none;
+            margin-bottom: 0;
+        }
+        .gray-container {
+          overflow:hidden;
+          background-color: #aaa;
+          width: 50%;
+          max-height:375px;
+          margin-right: 25px;
+          
+        }
+        .gray-container img{
+          height: 100%;
+          width: 100%;
+          object-fit: cover;
+
+        }
+
+        .recipe-details {
+            flex: 1;
+            text-align:left;
+        }
+
+        .recipe h2 {
+            margin: 0;
+            margin-bottom: 0.5rem;
+            font-size: 24px;
+        }
+
+        .recipe p {
+            margin: 0;
+            margin-bottom: 0.5rem;
+        }
+
+        .recipe button {
+            background-color: #222;
+            color: #fff;
+            display:flex;
+            border: none;
+            flex:1;
+            padding: 8px 16px;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+            width:40%;
+            transition: background-color 0.2s;
+        }
+        .navbar {
+            background-color: #222831;
+            padding: 10px;
+            display: flex;
+            align-items: center;
+            }
+
+        .recipe button:hover {
+            background-color: #444;
+        }
+        .custom{
+            display:flex;
+        }
+        .custom-details{
+           width:95%;
+        }
+    </style>
 </head>
-<style>
-  body {
-  font-family: Arial, sans-serif;
-  margin: 0;
-  padding: 0;
-}
-
-.navbar {
-  background-color: #393E46;
-  padding: 10px;
-  display: flex;
-  align-items: center;
-}
-
-.navbar .logo {
-  margin-right: auto;
-  padding: 5px;
-}
-
-.login-button {
-  border: none;
-  border-radius: 25px;
-  background-color: #222831;
-  color: #fff;
-  padding: 10px 20px;
-  font-size: 18px;
-  cursor: pointer;
-}
-
-.login-button:hover {
-  background-color: #424953;
-}
-
-.profile {
-  padding: 40px;
-}
-
-.container {
-  max-width: 800px;
-  margin: 0 auto;
-}
-
-.profile-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-.profile-header h1 {
-  font-size: 32px;
-}
-.edit-button {
-  border: none;
-  background-color: transparent;
-  color: #222831;
-  font-size: 16px;
-  cursor: pointer;
-}
-
-.profile-details {
-  display: flex;
-  align-items: center;
-  margin-top: 30px;
-}
-
-.profile-picture img {
-  width: 150px;
-  height: 150px;
-  border-radius: 50%;
-  object-fit: cover;
-  border: 5px solid #222831;
-  box-shadow: 0 4px 8px brown ;
-  margin-right: 30px;
-}
-
-.profile-info h2 {
-  font-size: 28px;
-  margin-bottom: 10px;
-}
-
-.profile-info p {
-  font-size: 18px;
-  margin: 5px 0;
-}
-</style>
 <body>
-    <nav class="navbar">
-        <a href="home.php" class="logo">
-            <img class="logo" src="images/FitHub.png" alt="FitHub Logo">
-        </a>
-        <button class="login-button">Connected as <?php echo $user['username']?></button>
-    </nav>
-    <main class="profile" style="background-color: #b9b9b9;">
-        <div class="container" >
-            <div class="profile-header" >
-                <h1>My Profile</h1>
-                <button class="edit-button">Edit Profile</button>
-                <a href ="logout.php" ><button class="edit-button">Log Out</button></a>
+<nav class="navbar">
+        <a href="#" class="logo"><img class="logo" src="images/FitHub.png"><img>  </a>
+        <a href="meal-maker.php"><button>Make A Meal</button></a>
+        <a href="recipes.php"><button>Recipes</button></a>
+        <a href="#" ><button>Training</button></a>
+        <!--{"Contact Us"}-->      
+  </nav>
+  <section class="section-content">
+    <div class="right-portion">
+        <h1>My Custom Meals:</h1>
+        <?php if (empty($customMeals)) { ?>
+            <!-- Display a button to add a custom meal when there are no custom meals -->
+            <h2>No custom meals found. <a href="meal-maker.php">Click here to add one</a>.</h2>
+        <?php } else { ?>
+            <!-- Display custom meals if available -->
+            <?php foreach ($customMeals as $customMeal) { ?>
+                <article class="custom">
+                    <div class="custom-details">
+                        <h2><?php echo $customMeal['meal_name']; ?></h2>
+                        <p><strong>Calories:</strong> <?php echo $customMeal['calories']; ?></p>
+                        <p><strong>Fat:</strong> <?php echo $customMeal['fat']; ?>g</p>
+                        <p><strong>Protein:</strong> <?php echo $customMeal['protein']; ?>g</p>
+                        <p><strong>Carbohydrates:</strong> <?php echo $customMeal['carbohydrates']; ?>g</p>
+                        <!-- Add other custom meal details here as needed -->
 
-            </div>
-            <div class="profile-details" >
-                <div class="profile-picture">
-                    <img src="images/cyb_enter.jpg" alt="User Profile Picture">
+                        <!-- Optionally, you can add a form to handle actions like editing or deleting the custom meal -->
+                        <form method="post" action="custom_meal.php">
+                            <input type="hidden" name="meal_id" value="<?php echo $customMeal['id']; ?>">
+                            <button type="submit">Edit</button>
+                            <input type="hidden" name="meal_id" value="<?php echo $customMeal['id']; ?>">
+                            <button type="submit">Edit</button>
+                        </form>
+                    </div>
+                </article>
+            <?php } ?>
+        <?php } ?>
+    </div>
+</section>
+
+<section class ="section-content">
+        <div class="left-portion">
+            <div class="container">
+                <?php foreach ($recipes as $recipe) { ?>
+                    <article class="recipe">
+                        <div class ="gray-container">
+                        <img src="<?php echo $recipe['image']; ?>" alt="<?php echo $recipe['recipe_name']; ?>">
                 </div>
-                <div class="profile-info">
-                    <h2><?php echo $fullName; ?></h2>
-                    <p>Email: <?php echo $email; ?></p>
-                    <p>Gender: <?php echo $gender; ?></p>
-                    <p>Birthday: <?php echo $birthday; ?></p>
-                    <p>Height: <?php echo $height; ?> cm</p>
-                    <p>Weight: <?php echo $weight; ?> kg</p>
-                </div>
+                        <div class="recipe-details">
+                            <h2><?php echo $recipe['recipe_name']; ?></h2>
+                            <p><strong>Description:</strong> <?php echo $recipe['description']; ?></p>
+                            <p><strong>Preparation Time:</strong> <?php echo $recipe['preparation_time']; ?> minutes</p>
+                            <p><strong>Cooking Time:</strong> <?php echo $recipe['cooking_time']; ?> minutes</p>
+                            <p><strong>Servings:</strong> <?php echo $recipe['servings']; ?></p>
+                            <p><strong>Calories:</strong> <?php echo $recipe['calories']; ?></p>
+                            <p><strong>Fat:</strong> <?php echo $recipe['fat']; ?>g</p>
+                            <p><strong>Protein:</strong> <?php echo $recipe['protein']; ?>g</p>
+                            <p><strong>Carbohydrates:</strong> <?php echo $recipe['carbohydrates']; ?>g</p>
+                                <button type="submit">Add to my meals</button>
+                        </div>
+                    </article>
+                <?php } ?>
             </div>
         </div>
-    </main>
+</section>
+
 </body>
 </html>
